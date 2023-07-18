006cd2e41c814df9f8d4c154e8542e8567568bbb
Robert Bubnik <rbubnik@gmail.com>
2023-07-17 23:30:21 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Rob';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
